518f2992d5c47f9f88e2624914e74b0bb19399c9
sozohoy <sozohoy@gmail.com>
2021-12-23 23:32:34 +0900

    Initial Commit

---
+++ uswTimeVersion1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uswTimeVersion1.0
+//
+//  Created by 한지석 on 2021/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
